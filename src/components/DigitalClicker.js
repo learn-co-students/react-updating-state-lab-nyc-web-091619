c9806c8f97f5aaeef2fbf54d373d174308f85b54
Danny Lee <dreamer@panix.com>
2019-11-19 16:32:41 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,39 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+
+    // set our attribute "timesClicked" to an initial value 
+    // of 0 
+    state = {
+        timesClicked: 0,
+    }
+
+    // when setState() it is passed an object which contains the
+    // "previousState" of our components state.  We take this value
+    // in an arrow function and then return an object which contains
+    // the attribute:value pair that adds one to the previous(current)
+    // value of the component's state attribute timeClicked.
+    onClickHandler = () => {
+        this.setState(previousState => {
+            // console.log(previousState);
+            return {
+                timesClicked: previousState.timesClicked + 1
+            }
+        })
+    }
+
+    // create a button and add an synthetic onClick event handler
+    // and set its call back function to onClickHandler.
+    // Also, set the innerHTML of our <h1> tags to show the number
+    // of times clicked (our state attribute)
+    render() {
+        return(
+            <div>
+                <button onClick={this.onClickHandler}>
+                    <h1>{this.state.timesClicked}</h1>
+                </button>
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,61 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+    state = {
+        errors: [],
+        user: null,
+        settings: {
+            bitrate: 8,
+            video: {
+            resolution: '1080p'
+            }
+        }
+    }
+
+    onBitrateClickHandler = () => {
+        // this.setState(previousState => {
+        //     settings: Object.assign(
+        //         this.previousState, 
+        //         this.settings, { bitrate: 12 }
+        //     )
+        // })
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12,
+            }
+        });
+    }
+
+    onResolutionClickHandler = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings, 
+                video: {
+                    ...this.state.settings.video,
+                    resolution: '720p',
+                }
+            }
+        });
+    }
+
+    // Create a button with the class 'bitrate'. Clicking this button changes 
+    //      the settings.bitrate state property to 12.
+
+    // Create a button with the class 'resolution'. Clicking this button changes 
+    //      the settings.video.resolution state property to '720p'.
+
+    render() {
+        return(
+            <div>
+                <button className="bitrate" onClick={this.onBitrateClickHandler}>
+                    Bitrate
+                </button>
+                <button className="resolution" onClick={this.onResolutionClickHandler}>
+                    Resolution
+                </button>
+            </div>
+        )
+    }
+}
